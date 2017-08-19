13934d17d9208598d88dbe5511dae6cd4fa47dd8
Paul Van Oosten <paulvoosten@protonmail.com>
2017-08-19 17:53:44 +0200

    Update

--- config/polybar/modules/packages.sh
+++ config/polybar/modules/packages.sh
@@ -4,7 +4,5 @@ aur=$(cower -u|wc -l)
 check=$((pac + aur))
 
 if [[ "$check" != "0" ]]; then
-    echo "%{F#585858}%{F-} $pac $aur"
-else
-   echo "%{F#585858}%{F-} Up to date"
+    echo "$pac $aur"
 fi
