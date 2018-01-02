0d059d425aef889ba881745af0b25d9c5ce2719f
Paul Van Oosten <paulvoosten@protonmail.com>
2018-01-02 15:37:56 +0100

    Remove quotes

--- wm/.config/polybar/modules/compton-toggle.sh
+++ wm/.config/polybar/modules/compton-toggle.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-if pgrep -x "compton" > /dev/null; then
-    killall compton
+if pgrep -x compton > /dev/null; then
+    pkill -x compton
 else
     compton -b
 fi
--- wm/.config/polybar/modules/compton.sh
+++ wm/.config/polybar/modules/compton.sh
@@ -2,7 +2,7 @@
 icon=
 color=$(xrdb -query|grep 'color8:'|cut -f2)
 
-if pgrep -x "compton" > /dev/null; then
+if pgrep -x compton > /dev/null; then
     color=$(xrdb -query|grep 'color2:'|cut -f2)
 fi
 
